e4bffa2957a44242b28a48a141783fc6f05e72e0
Bianca Ribeiro de Souza <bianca.ribeiro@ges.inatel.br>
2023-11-23 14:13:00 -0300

    Adicionar rascunho da main

--- Nexus/src/main/java/Main.java
+++ Nexus/src/main/java/Main.java
@@ -1,3 +1,30 @@
+import program.DAO.Model.Genero;
+
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+
+        System.out.println("Olá, seja bem vindo a nossa loja de jogos: a Nexus Store!");
+        System.out.println("Antes de começarmos, precisamos cadastrar pelo menos um gênero de jogo.\nQuando se cansar, basta inserir a palavra ACABOU.");
+
+        boolean inserirGenero = true;
+        String nomeGenero;
+        int idGenero;
+
+        Scanner entrada = new Scanner(System.in);
+
+        while(true) {
+            nomeGenero = entrada.next();
+
+            if (Objects.equals(nomeGenero, "ACABOU")) {
+                break;
+            }
+
+            idGenero = entrada.nextInt();
+            new Genero(nomeGenero, idGenero);
+        }
+
+    }
 }
---
+++ Nexus/src/main/java/program/DAO/Model/Genero.java
@@ -0,0 +1,13 @@
+package program.DAO.Model;
+
+public class Genero {
+
+    private String nome;
+    private int idGenero;
+
+    public Genero (String nome, int idGenero) {
+        this.nome = nome;
+        this.idGenero = idGenero;
+    }
+
+}
